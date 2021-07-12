666fa7b00d9e27c5a807881e397edc4aee930230
Lado <Tsivtsivadzelado@yahoo.com>
2021-07-12 02:25:42 -0700

    adding groups and files

---
+++ LadoTsivtsivadze_51/Presentation/Scenes/Feed/View/FeedController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedController.swift
+//  LadoTsivtsivadze_51
+//
+//  Created by Ladolado3911 on 12.07.21.
+//
+
+import UIKit
+
+class FeedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
