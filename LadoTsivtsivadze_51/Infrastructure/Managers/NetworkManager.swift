2ebb503ec4e4fe32a79f94c12464c262ff355f89
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-13 09:14:48 -0700

    added network manager

--- LadoTsivtsivadze_51/Infrastructure/Managers/NetworkManager.swift
+++ LadoTsivtsivadze_51/Infrastructure/Managers/NetworkManager.swift
@@ -8,16 +8,24 @@
 import Foundation
 
 struct Endpoints {
-    
+    var booksEndpoint: String = "https://private-526b6d-bookstore.apiary-mock.com/data/books"
 }
 
 protocol NetworkManagerProtocol: AnyObject {
-    func get<T: Codable>(with url: String, completion: ((Result<T, Error>) -> Void))
+    func get<T: Codable>(with url: String, completion: @escaping ((Result<T, Error>) -> Void))
 }
 
 final class NetworkManager: NetworkManagerProtocol {
-    func get<T: Codable>(with url: String, completion: ((Result<T, Error>) -> Void)) {
-        //let response = URLSession.shared.dataTask(with: <#T##URL#>)
+    func get<T: Codable>(with url: String, completion: @escaping ((Result<T, Error>) -> Void)) {
+        URLSession.shared.dataTask(with: URL(string: url)!) {data, response, error in
+            do {
+                let items = try JSONDecoder().decode(T.self, from: data!)
+                completion(.success(items))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
