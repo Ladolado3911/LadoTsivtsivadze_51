0877607fa5a5f4ed098c485aff39d5536afac448
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-13 09:02:58 -0700

    added book model

--- LadoTsivtsivadze_51/Infrastructure/Managers/Models.swift
+++ LadoTsivtsivadze_51/Infrastructure/Managers/Models.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
-struct BookCovers {
+struct Book: Codable {
+    var title: String?
+    var author: String?
+    var summary: String?
+    var image: String?
+}
+
+struct BookModel {
     
+    var book: Book!
+    
+    var title: String {
+        return book.title ?? ""
+    }
+    
+    var author: String {
+        return book.author ?? ""
+    }
+    
+    var summary: String {
+        return String((book.summary ?? "").prefix(50))
+    }
+    
+    var image: URL {
+        return URL(string: book.image ?? "")!
+    }
+    
+    init(using model: Book) {
+        book = model
+    }
 }
